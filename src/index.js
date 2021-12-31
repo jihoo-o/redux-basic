7fb7650bc1dd1003a0dc141fd41daa64b86247cc
kmseonhwa <seonhwakei@gmail.com>
2021-12-31 10:14:08 +0900

    Add: Redux counter

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
+import { createStore } from 'redux';
 const plus = document.querySelector('.plus');
 const minus = document.querySelector('.minus');
 const result = document.querySelector('.result');
 
-let count = 0;
-result.innerText = count;
+// Actions
+const ADD = 'ADD';
+const MINUS = 'MINUS';
 
-const updateResult = () => {
-    result.innerText = count;
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case ADD:
+            return state + 1;
+        case MINUS:
+            return state - 1;
+        default:
+            return state;
+    }
 };
 
 const handleAdd = () => {
-    count = count + 1;
-    updateResult();
+    countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-    count = count - 1;
-    updateResult();
+    countStore.dispatch({ type: MINUS });
+};
+
+/**
+ * Observer
+ * The observer object should have a `next` method?
+ */
+const onChange = () => {
+    result.innerText = countStore.getState();
 };
 
 plus.addEventListener('click', handleAdd);
 minus.addEventListener('click', handleMinus);
+
+const countStore = createStore(reducer);
+const unsubscribe = countStore.subscribe(onChange);
