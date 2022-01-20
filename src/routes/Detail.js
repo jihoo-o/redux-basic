70ca524beccee504cb964f483ab01e3559828218
kmseonhwa <seonhwakei@gmail.com>
2022-01-20 16:12:31 +0900

    Add: Detail page - delete button

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,26 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
+import { actionCreators } from '../store';
 
-const Detail = ({ toDos }) => {
+const Detail = ({ toDos, onBtnClick }) => {
     const navigate = useNavigate();
     const { id } = useParams();
-    const todo = toDos.find((todo) => todo.id === parseInt(id));
+    const [todo, setTodo] = useState(
+        toDos.find((todo) => todo.id === parseInt(id))
+    );
 
     useEffect(() => {
         if (!todo) {
             navigateToHome();
         }
-    }, []);
+    }, [todo]);
 
     const navigateToHome = () => {
         navigate('/');
+    };
+
+    const handleDelete = () => {
+        onBtnClick(id);
+        setTodo(null);
     };
 
     return (
         <>
             <h1>{todo && todo.text}</h1>
             <h3>created at {todo?.id}</h3>
+            <button onClick={() => handleDelete()}>X</button>
             <button onClick={navigateToHome}>back</button>
         </>
     );
@@ -30,4 +39,12 @@ const mapStateToProps = (state) => {
     return { toDos: state };
 };
 
-export default connect(mapStateToProps)(Detail);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onBtnClick: (id) => {
+            dispatch(actionCreators.deleteToDo(parseInt(id)));
+        },
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
