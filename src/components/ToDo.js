367236770f493382b8547e71b207a507aef2913a
kmseonhwa <seonhwakei@gmail.com>
2022-01-20 15:59:32 +0900

    Add: Detail page

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { actionCreators } from '../store';
 
-const ToDo = ({ text, onBtnClick }) => {
+const ToDo = ({ id, text, onBtnClick }) => {
     return (
         <li>
-            {text}
+            <Link to={`/${id}`}>{text}</Link>
             <button onClick={onBtnClick}>X</button>
         </li>
     );
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,33 @@
-export default () => 'Detail';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const Detail = ({ toDos }) => {
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const todo = toDos.find((todo) => todo.id === parseInt(id));
+
+    useEffect(() => {
+        if (!todo) {
+            navigateToHome();
+        }
+    }, []);
+
+    const navigateToHome = () => {
+        navigate('/');
+    };
+
+    return (
+        <>
+            <h1>{todo && todo.text}</h1>
+            <h3>created at {todo?.id}</h3>
+            <button onClick={navigateToHome}>back</button>
+        </>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return { toDos: state };
+};
+
+export default connect(mapStateToProps)(Detail);
